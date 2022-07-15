6c46d6ccd3bb8ec9276f1ac9ee130d6f494242e7
mapleleaf-jian <1028517695@qq.com>
2022-07-15 11:33:07 +0800

    ref属性

---
+++ 01_src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <School></School>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import School from './components/School'
+
+export default {
+  name: 'App',
+  components: {
+    School
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
