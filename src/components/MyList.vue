64392cd95be5c4a6e276e442efc249a54b97518b
mapleleaf-jian <1028517695@qq.com>
2022-07-15 20:17:58 +0800

    todolist拆页面

---
+++ src/components/MyList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul class="todo-main">
+      <MyItem/>
+      <MyItem/>
+      <MyItem/>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import MyItem from "@/components/MyItem";
+  export default {
+    name: 'MyList',
+    components: {MyItem}
+  }
+</script>
+
+<style scoped>
+  /*main*/
+  .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
