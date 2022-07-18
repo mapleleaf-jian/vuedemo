9b97a1335e4bc7eb12d4d20085068ff2b520aa15
mapleleaf-jian <1028517695@qq.com>
2022-07-18 13:34:01 +0800

    动画效果

---
+++ src/components/TestHello.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">展示/隐藏</button>
+    <transition appear name="hello">
+      <h3 v-show="isShow" >hello</h3>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestHello',
+  data() {
+    return {
+      isShow: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h3 {
+    background-color: orange;
+  }
+
+  .hello-enter-active {
+    animation: demo 1s; /*表示进来的动画*/
+  }
+
+  .hello-leave-active {
+    animation: demo 1s reverse; /*表示出去的动画*/
+  }
+
+  /*添加关键帧*/
+  @keyframes demo { /*demo是动画名*/
+    from {
+      transform: translateX(-100%);
+    }
+    to {
+      transform: translateX(0px);
+    }
+  }
+</style>
