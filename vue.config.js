5733a483ce1a71ba62257ee27accf5c425f86cf0
mapleleaf-jian <1028517695@qq.com>
2022-07-19 15:06:29 +0800

    配置代理

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  // 开启代理服务器
+  // devServer: {
+  //   proxy: 'http://localhost:5000'
+  // }
+  devServer: {
+    proxy: {
+      '/api1': { // 匹配所有以/api1开头的路径
+        target: 'http://localhost:5000', // 代理目标的基础路径
+        // 表示将 以api1 开头的请求中的api1替换成''，即在最终请求的时候去掉/api1
+        pathRewrite: {'^/api1': ''},
+        ws: true, // 表支持websocket,默认值为true
+        changeOrigin: true // 默认值为true
+      },
+      '/api2': {
+        target: 'http://localhost:5001',
+        pathRewrite: {'^/api2': ''},
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
 })
