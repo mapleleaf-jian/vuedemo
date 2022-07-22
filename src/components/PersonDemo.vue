9ef7fef23b36be13788689269bf1cd069a6ae85a
mapleleaf-jian <1028517695@qq.com>
2022-07-22 17:19:19 +0800

    多组件共享数据

---
+++ src/components/PersonDemo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p style="color: skyblue">Count组件的和为：{{sum}}</p>
+    <input type="text" placeholder="请输入姓名" v-model="name">
+    <button @click="addPerson()">添加</button>
+    <ul>
+      <li v-for="p of persons" :key="p.id">{{p.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+import {nanoid} from 'nanoid'
+export default {
+  name: 'PersonDemo',
+  data() {
+    return {
+      name: ''
+    }
+  },
+  computed: {
+    ...mapState(['persons', 'sum'])
+  },
+  methods: {
+    addPerson() {
+      const person = {id: nanoid(), name: this.name}
+      this.$store.commit('ADD_PERSON', person)
+      this.name = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
