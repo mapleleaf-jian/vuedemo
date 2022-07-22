5703f6e15c55d4446f051fe04794943caa9d210c
mapleleaf-jian <1028517695@qq.com>
2022-07-22 16:31:15 +0800

    mapActions 和 mapMutations

---
+++ 26_src_mapActions和mapMutations/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <CountDemo/>
+  </div>
+</template>
+
+<script>
+  import CountDemo from './components/CountDemo'
+
+  export default {
+    name: 'App',
+    components: {CountDemo}
+  }
+</script>
---
+++ 26_src_mapActions和mapMutations/store/index.js
@@ -0,0 +1,66 @@
+// 创建vuex中的store
+// 引入Vuex 和 Vue
+import Vuex from 'vuex'
+import Vue from "vue";
+
+// 安装Vuex
+Vue.use(Vuex)
+
+// actions 用于响应组件中的动作，用于写具体的业务逻辑
+const actions = {
+    add(context, value) {
+        console.log('actions中的add被调用了')
+        context.commit('ADD', value)
+    },
+    sub(context, value) {
+        console.log('actions中的sub被调用了')
+        context.commit('SUB', value)
+    },
+    addOdd(context, value) {
+        console.log('actions中的addOdd被调用了')
+        console.log(context)
+        if (context.state.sum % 2) {
+            context.commit('ADD', value)
+        }
+    },
+    addWait(context, value) {
+        console.log('actions中的addWait被调用了')
+        setTimeout(() => {
+            context.commit('ADD', value)
+        }, 500)
+    }
+}
+
+// mutations 用于操作数据(state)，用于写操作
+const mutations = {
+    ADD(state, value) {
+        console.log(state)
+        console.log('mutations中的ADD被调用了')
+        state.sum += value
+    },
+    SUB(state, value) {
+        console.log('mutations中的SUB被调用了')
+        state.sum -= value
+    }
+}
+
+// state 用于存储数据
+const state = {
+    sum: 0, // 总和
+    address: 'beijing'
+}
+
+// getters，用于加工数据
+const getters = {
+    bigSum(state) {
+        return state.sum * 10
+    }
+}
+
+// 创建并暴露store
+export default new Vuex.Store({
+    actions,
+    mutations,
+    state,
+    getters
+})
