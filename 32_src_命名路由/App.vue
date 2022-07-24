35b5541141b9d29f834cc2c734bc6734c81cb709
mapleleaf-jian <1028517695@qq.com>
2022-07-24 22:17:58 +0800

    命名路由

---
+++ 32_src_命名路由/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div>
+      <div class="row">
+        <Banner/>
+      </div>
+      <div class="row">
+        <div class="col-xs-2 col-xs-offset-2">
+          <div class="list-group">
+            <router-link to="/about" class="list-group-item" active-class="active">About</router-link>
+            <router-link to="/home" class="list-group-item" active-class="active">Home</router-link>
+          </div>
+        </div>
+        <div class="col-xs-6 panel panel-body">
+            <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from './components/Banner'
+  export default {
+    name: 'App',
+    components: {Banner}
+  }
+</script>
