4634631b7c02b3db1966325fbe8d602688794527
mapleleaf-jian <1028517695@qq.com>
2022-07-25 19:16:46 +0800

    独享守卫

---
+++ 40_src_独享守卫/pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>我是About的内容</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  beforeRouteEnter(to, from, next) {
+
+  },
+  beforeRouteLeave(to, from, next) {
+
+  }
+}
+</script>
