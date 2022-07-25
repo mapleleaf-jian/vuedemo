27dd0494cdac33c340cdcb1a2a9711a52d979449
mapleleaf-jian <1028517695@qq.com>
2022-07-25 17:23:28 +0800

    路由后置守卫

---
+++ 39_src_路由后置守卫/pages/News.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ul>
+      <li :style="{opacity}">vue</li>
+      <li>news001 <input type="text"></li>
+      <li>news002 <input type="text"></li>
+      <li>news003 <input type="text"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'News',
+  data() {
+    return {
+      opacity: 1,
+      timer: null
+    }
+  },
+  activated() {
+    this.timer = setInterval(() => {
+      console.log('@')
+      this.opacity -= 0.01
+      if (this.opacity <= 0) this.opacity = 1
+    }, 16)
+  },
+  deactivated() {
+    clearInterval(this.timer)
+  }
+}
+</script>
